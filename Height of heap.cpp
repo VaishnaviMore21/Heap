34eaa3d30ccc5cd8b1ae0180432f5d6aafe003db
Vaishnavi More <143414333+VaishnaviMore21@users.noreply.github.com>
2025-02-13 00:48:34 +0530

    Create Height of heap.cpp

---
+++ Height of heap.cpp
@@ -0,0 +1,18 @@
+
+class Solution{
+public:
+    int heapHeight(int N, int arr[]){
+        // code here
+        if(N==1)
+        {
+            return 1;
+        }
+        int height=0;
+        while(N>1)
+        {
+            height++;
+            N/=2;
+        }
+        return height;
+    }
+};
